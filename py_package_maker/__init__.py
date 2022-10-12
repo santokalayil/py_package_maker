afc724a2d5f083c3fa4401fe7b8a8b1c6f335663
ezetap <santokalayil@gmail.com>
2022-10-12 19:54:01 +0530

    __version__ variable section added in __init__.py

--- py_package_maker/__init__.py
+++ py_package_maker/__init__.py
@@ -7,5 +7,9 @@ def initializer():
     license.create_license_file()
     readme.create_readme_file()
     setup.create_setup_py_file()
+
+    # create MANIFEST.in file
+    # .temp file exclusion 
+
     package_dir_n_init.create_src_dir_n_init_file()
 
